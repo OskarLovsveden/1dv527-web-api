7d96229d1bcda609cc8afb55b1d1c94b8931cc46
ol222hf <ol222hf@student.lnu.se>
2021-02-26 14:20:31 +0100

    adding basic express server

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('./config/mongoose')
+const app = express()
+
+mongoose.connect().catch(error => {
+    console.error(error)
+    process.exit(1)
+})
+
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.use('/', (req, res, next) => res.send('test'))
+
+const PORT = 8000
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+})
