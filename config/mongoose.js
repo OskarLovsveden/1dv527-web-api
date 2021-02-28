52beff64df76a3a73f4b386be05f6d0373db8608
ol222hf <ol222hf@student.lnu.se>
2021-02-28 18:49:43 +0100

    import instead of require

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,25 +1,18 @@
-const mongoose = require('mongoose')
+import { connection, connect } from 'mongoose'
 
-/**
- * Establishes a connection to a database.
- *
- * @returns {Promise} Resolves to this if connection succeeded.
- */
-module.exports.connect = async () => {
-    mongoose.connection.on('connected', () => console.log('Mongoose connection is open.'))
-    mongoose.connection.on('error', err => console.error(`Mongoose connection error has occurred: ${err}`))
-    mongoose.connection.on('disconnected', () => console.log('Mongoose connection is disconnected.'))
+export const connectDB = async () => {
+    connection.on('connected', () => console.log('Mongoose connection is open.'))
+    connection.on('error', err => console.error(`Mongoose connection error has occurred: ${err}`))
+    connection.on('disconnected', () => console.log('Mongoose connection is disconnected.'))
 
-    // If the Node process ends, close the Mongoose connection.
     process.on('SIGINT', () => {
-        mongoose.connection.close(() => {
+        connection.close(() => {
             console.log('Mongoose connection is disconnected due to application termination.')
             process.exit(0)
         })
     })
 
-    // Connect to the server.
-    return mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    return connect(process.env.DB_CONNECTION_STRING, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true
