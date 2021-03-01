6b209e1148712598bd9f465f2072af3bc57071f0
ol222hf <ol222hf@student.lnu.se>
2021-03-01 14:11:36 +0100

    include animal router

--- routes/api/v1/router.js
+++ routes/api/v1/router.js
@@ -1,23 +1,11 @@
 import express from 'express'
 import { router as accountRouter } from './account-router.js'
-import { router as usersRouter } from './user-router.js'
-// import { router as tasksRouter } from './tasks-router.js'
+import { router as userRouter } from './user-router.js'
+import { router as animalRouter } from './animal-router.js'
 
 export const router = express.Router()
 
 router.get('/', (req, res) => res.json({ message: 'Lovsveden API v1' }))
 router.use('/', accountRouter)
-router.use('/users', usersRouter)
-// router.use('/animals', animalsRouter)
-
-// TODO
-
-// POST /users - add user
-// POST /login
-// POST /logout
-
-// GET /fishes - gets all catches
-// POST /fishes - adds new catch
-// PUT /fishes/:id - updates info on a catch
-// DELETE /fishes/:id - deletes catch
-
+router.use('/users', userRouter)
+router.use('/animals', animalRouter)
