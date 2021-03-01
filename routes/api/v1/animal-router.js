eb0f292ff56968d6be4fdbca0d32b44f445233f1
ol222hf <ol222hf@student.lnu.se>
2021-03-01 14:15:59 +0100

    route with jwt authentication

---
+++ routes/api/v1/animal-router.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
+import { AnimalController } from '../../../controller/animal-controller.js'
+
+export const router = express.Router()
+
+const controller = new AnimalController()
+
+const authenticateJWT = (req, res, next) => {
+    const authorization = req.headers.authorization?.split(' ')
+
+    if (authorization?.[0] !== 'Bearer') {
+        next(createError(401))
+        return
+    }
+
+    try {
+        const payload = jwt.verify(authorization[1], process.env.ACCESS_TOKEN_SECRET)
+        req.user = {
+            username: payload.username
+        }
+
+        next()
+    } catch (err) {
+        next(createError(403))
+    }
+}
+
+router.param('id', (req, res, next, id) => controller.loadAnimal(req, res, next, id))
+
+// GET animals
+router.get('/',
+    authenticateJWT,
+    (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ),
+    (req, res, next) => controller.findAll(req, res, next)
+)
+
+// GET animals/:id
+router.get('/:id',
+    authenticateJWT,
+    (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ),
+    (req, res, next) => controller.find(req, res, next)
+)
+
+// POST animals
+router.post('/',
+    authenticateJWT,
+    (req, res, next) => hasPermission(req, res, next, PermissionLevels.CREATE),
+    (req, res, next) => controller.create(req, res, next)
+)
+
+// PUT animals/:id
+router.put('/:id',
+    authenticateJWT,
+    (req, res, next) => hasPermission(req, res, next, PermissionLevels.UPDATE),
+    (req, res, next) => controller.update(req, res, next)
+)
+
+// DELETE animals/:id
+router.delete('/:id',
+    authenticateJWT,
+    (req, res, next) => hasPermission(req, res, next, PermissionLevels.DELETE),
+    (req, res, next) => controller.delete(req, res, next)
+)
