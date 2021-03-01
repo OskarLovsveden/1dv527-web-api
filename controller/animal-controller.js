79f8a9948b13e8fe9043a2181746ceb5f616ccd3
ol222hf <ol222hf@student.lnu.se>
2021-03-01 14:13:55 +0100

    crud functionality added

---
+++ controller/animal-controller.js
@@ -0,0 +1,94 @@
+import createError from 'http-errors'
+import { Animal } from '../models/Animal.js'
+
+export class AnimalController {
+
+    async loadAnimal(req, res, next, id) {
+        try {
+            const animal = Animal.getById(id)
+
+            if (!animal) {
+                next(createError(404))
+                return
+            }
+
+            req.animal = animal
+            next()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async find(req, res, next) {
+        res.json(req.animal)
+    }
+
+    async findAll(req, res, next) {
+        try {
+            const animals = Animal.getAll()
+            res.json(animals)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async create(req, res, next) {
+        try {
+            const animal = await Animal.insert({
+                rescuer: req.body.rescuer,
+                name: req.body.name,
+                position: {
+                    latitude: req.body.position.latitude,
+                    longitude: req.body.position.longitude
+                },
+                facility: req.body.facility,
+                city: req.body.city,
+                species: req.body.species,
+                weight: req.body.weight,
+                length: req.body.length,
+                image: req.body.image
+            })
+
+            res.status(201)
+            res.json(animal)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async update(req, res, next) {
+        try {
+            await req.animal.update({
+                rescuer: req.body.rescuer,
+                name: req.body.name,
+                position: {
+                    latitude: req.body.position.latitude,
+                    longitude: req.body.position.longitude
+                },
+                facility: req.body.facility,
+                city: req.body.city,
+                species: req.body.species,
+                weight: req.body.weight,
+                length: req.body.length,
+                image: req.body.image
+            })
+
+            res.status(204)
+            res.end()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            await req.animal.delete()
+
+            res.status(204)
+            res.end()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+}
