2f30460af4f66d4f041fe0d750cdda0467935709
ol222hf <ol222hf@student.lnu.se>
2021-03-01 14:13:04 +0100

    functionality load/find user

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,31 +2,25 @@ import createError from 'http-errors'
 import { User } from '../models/User.js'
 
 export class UserController {
-
-    async register(req, res, next) {
+    async loadUser(req, res, next, id) {
         try {
-            console.log(req.body)
-            const user = await User.insert({
-                username: req.body.username,
-                password: req.body.password
-            })
+            const user = await User.getById(id)
 
-            res.status(201).json({ id: user.id })
-
-        } catch (error) {
-            let err = error
-
-            if (err.code === 11000) {
-                // Duplicated keys.
-                err = createError(409)
-                err.innerException = error
-            } else if (error.name === 'ValidationError') {
-                // Validation error(s).
-                err = createError(400)
-                err.innerException = error
+            if (!user) {
+                next(createError(404))
+                return
             }
 
-            next(err)
+            req.user = user
+
+            next()
+        } catch (error) {
+            next(error)
         }
     }
+
+    async find(req, res, next) {
+        res.json(req.user)
+    }
+
 }
