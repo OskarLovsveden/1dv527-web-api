2e0bfba732cde018ab2fb7378e4ff6978ec81220
ol222hf <ol222hf@student.lnu.se>
2021-03-02 19:29:22 +0100

    notify subscribers

---
+++ utils/webhook.js
@@ -0,0 +1,27 @@
+import { Webhook } from '../models/Webhook.js'
+import axios from 'axios'
+
+export const send = async (data) => {
+
+    const webhooks = await Webhook.find({})
+
+    for (const webhook of webhooks) {
+        try {
+            const res = await axios({
+                url: webhook.url,
+                method: 'POST',
+                timeout: 5000,
+                data: data
+            })
+
+            if (res.status === 200) {
+                console.log(`Webhook sent to ${webhook.url}`)
+            }
+
+        } catch (error) {
+            console.error('Error: ' + error.message)
+            webhook.errorCounter++
+            await webhook.save()
+        }
+    }
+}
