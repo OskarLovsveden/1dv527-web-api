7b72d244e57447ce79e9bbf6b532f8497abdd303
ol222hf <ol222hf@student.lnu.se>
2021-03-02 23:15:13 +0100

    breaking out jwt auth

--- routes/api/v1/webhook-router.js
+++ routes/api/v1/webhook-router.js
@@ -1,32 +1,10 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
-import createError from 'http-errors'
 import { WebhookController } from '../../../controller/webhook-controller.js'
+import { authenticateJWT } from '../../../helpers/auth.js'
 
 export const router = express.Router()
 
 const controller = new WebhookController()
 
-const authenticateJWT = (req, res, next) => {
-    const authorization = req.headers.authorization?.split(' ')
-
-    if (authorization?.[0] !== 'Bearer') {
-        next(createError(401))
-        return
-    }
-
-    try {
-        const payload = jwt.verify(authorization[1], process.env.ACCESS_TOKEN_SECRET)
-
-        req.user = {
-            username: payload.username
-        }
-
-        next()
-    } catch (err) {
-        next(createError(403))
-    }
-}
-
-// POST webhooks - add webhook
+// POST add webhook
 router.post('/', authenticateJWT, (req, res, next) => controller.create(req, res, next))
