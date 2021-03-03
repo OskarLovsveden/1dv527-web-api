cbfb47fa6e82aa95436ff8124b17dfe77d814bd4
ol222hf <ol222hf@student.lnu.se>
2021-03-03 01:56:39 +0100

    auth and permission checks added

--- routes/api/v1/user-router.js
+++ routes/api/v1/user-router.js
@@ -1,10 +1,20 @@
 import express from 'express'
 import { UserController } from '../../../controller/user-controller.js'
+import { authenticateJWT } from '../../../helpers/auth.js'
+import { PermissionLevels, hasPermission } from '../../../helpers/permission.js'
 
 export const router = express.Router()
 
 const controller = new UserController()
 
 router.param('id', (req, res, next, id) => controller.loadUser(req, res, next, id))
-router.get('/:id', (req, res, next) => controller.find(req, res, next))
-router.get('/', (req, res, next) => controller.findAll(req, res, next))
+
+router.get('/:id',
+    authenticateJWT,
+    (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ),
+    (req, res, next) => controller.find(req, res, next))
+
+router.get('/',
+    authenticateJWT,
+    (req, res, next) => hasPermission(req, res, next, PermissionLevels.ADMIN),
+    (req, res, next) => controller.findAll(req, res, next))
