282ae3fc337377490af14a8e1f50822998516f09
ol222hf <ol222hf@student.lnu.se>
2021-03-03 17:52:33 +0100

    cleanup comments

--- routes/api/v1/animal-router.js
+++ routes/api/v1/animal-router.js
@@ -9,31 +9,26 @@ const controller = new AnimalController()
 
 router.param('id', (req, res, next, id) => controller.loadAnimal(req, res, next, id))
 
-// GET animals
 router.get('/',
     authenticateJWT,
     (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ),
     (req, res, next) => controller.findAll(req, res, next))
 
-// GET animals/:id
 router.get('/:id',
     authenticateJWT,
     (req, res, next) => hasPermission(req, res, next, PermissionLevels.READ),
     (req, res, next) => controller.find(req, res, next))
 
-// POST animals
 router.post('/',
     authenticateJWT,
     (req, res, next) => hasPermission(req, res, next, PermissionLevels.CREATE),
     (req, res, next) => controller.create(req, res, next))
 
-// PUT animals/:id
 router.put('/:id',
     authenticateJWT,
     (req, res, next) => hasPermission(req, res, next, PermissionLevels.UPDATE),
     (req, res, next) => controller.update(req, res, next))
 
-// DELETE animals/:id
 router.delete('/:id',
     authenticateJWT,
     (req, res, next) => hasPermission(req, res, next, PermissionLevels.DELETE),
